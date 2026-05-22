7417bf14850ed1e74617de4aeba37dda7efabc33
toys69 <toys69@gmail.com>
2026-05-22 16:57:07 +0800

    feat(unsafeconv): StringToBytes,BytesToString

---
+++ unsafeconv/unsafe_conv.go
@@ -0,0 +1,10 @@
+package unsafeconv
+
+import "unsafe"
+
+func StringToBytes(s string) []byte {
+	return unsafe.Slice(unsafe.StringData(s), len(s))
+}
+func BytesToString(b []byte) string {
+	return unsafe.String(unsafe.SliceData(b), len(b))
+}
